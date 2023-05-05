8f8ee0d6de923c4a2b58bbd4e6b34eba8c5a8bc3
Mike <nhj25252@gmail.com>
2023-05-05 23:41:53 +0900

    append new node as last

--- linkedlist.js
+++ linkedlist.js
@@ -1,28 +1,52 @@
+#!/usr/bin/env node
+
 class LinkedListClass {
   // list is empty , it's pointing to null
-  constructor(head = null) {
-    this.head = head;
+  // initial linkelist
+  constructor(data = null) {
+    this.head = data;
+    this.size = 0;
   }
-  append(data) {
-    // append as the last node
-    // list's head is now this new node containing data
-    this.head = Node(data); // nextNode is null by default = it's the last node.
+
+  append(nodeName, data) {
+    let current = this.head; // point to head first
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    current.nextNode = Node(nodeName, data);
+    console.log(current);
+
+    this.size++;
   }
-  prepend(data) {
-    // prepend as the head
-    this.head = Node(data, this.head); // this new node is pointing to the head (it's head now)
+  prepend(nodeName, data) {
+    // prepend as the list's head
+    this.head = Node(nodeName, data, this.head); // this new node is pointing to the head (it's head now)
+    this.size++;
+  }
+  getSize() {
+    return this.size;
+  }
+  getHead() {
+    return this.head;
   }
 }
-const linkedlistClass = new LinkedListClass();
-linkedlistClass.append("some data");
-linkedlistClass.prepend("head now!");
-console.log("list", linkedlistClass);
+const linkedList = new LinkedListClass();
+
+linkedList.prepend(" node", 1);
+linkedList.prepend("node", 0);
+linkedList.prepend("head node", "head");
+linkedList.append("last node", 3);
+// linkedList.append("real last node", 4);
+console.log(linkedList);
+// console.log("list", linkedList);
+// console.log("getSize", linkedList.getSize());
+// console.log("getSize", linkedList.getHead());
 
 // Node factory
 // nextNode is null by default unless specified.
-function Node(data, nextNode = null) {
+function Node(name, data, nextNode = null) {
   // not pointing at anything unless specified.
   // is an object containing key 'data', and key 'nextNode'
-  return { data, nextNode };
+  return { name, data, nextNode };
 }
 // node1 object
